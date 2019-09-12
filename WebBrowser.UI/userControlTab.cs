203a05521b206b295e4b8e6e4cda5e2ad170e540
anthonyjmarchi <marchianthony@gmail.com>
2019-09-12 22:38:35 +0200

    Added the functionality for both the forward and backward buttons.

--- WebBrowser.UI/userControlTab.cs
+++ WebBrowser.UI/userControlTab.cs
@@ -15,6 +15,7 @@ namespace WebBrowser.UI
         public Stack<string> backStack = new Stack<string>();
         public Stack<string> forwardStack = new Stack<string>();
         
+        
             public userControlTab()
         {
             InitializeComponent();
@@ -36,13 +37,48 @@ namespace WebBrowser.UI
             if (e.KeyCode == Keys.Enter)
             {
                 webBrowser1.Navigate(urlBar.ToString());
+                backStack.Push(urlBar.ToString());
             }
+        
         }
 
         private void toolStripButton3_Click(object sender, EventArgs e)
-        {
+        {   
             webBrowser1.Navigate(urlBar.ToString());
         }
 
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (backStack.Count == 0)
+            {
+                webBrowser1.Navigate(urlBar.ToString());
+            }
+            else
+            {
+                forwardStack.Push(urlBar.ToString());
+                String b = backStack.Pop().ToString();
+                if (b == urlBar.Text)
+                {
+                    b = backStack.Pop().ToString();
+                }
+                webBrowser1.Navigate(b);
+                urlBar.Text = b;
+            }  
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (forwardStack.Count == 0)
+            {
+                webBrowser1.Navigate(urlBar.ToString());
+            }
+            else {
+                backStack.Push(urlBar.ToString());
+                String a = forwardStack.Pop().ToString();
+                webBrowser1.Navigate(a);
+                urlBar.Text = a;
+            }
+       
+        }
     }
 }
